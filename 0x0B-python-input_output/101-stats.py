7af10ec5fc8542c4225426b050755569a5bfbb67
Samwel Nyandoro <samwelnyandoro1@gmail.com>
2023-07-11 07:19:57 -0700

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,50 +1,90 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics
+"""
+0x0B. Python - Input/Output, task 16. Log parsing
+
+Parses a log of HTTP GET request results from stdin to tabulate the total
+counts of status codes appearing in each response, and the total file size
+across all requests.
+
+Example of expected log line input:
+128.230.61.246 - [2017-02-05 23:31:23.258076] \
+"GET /projects/260 HTTP/1.1" 301 292
+
+Fields:
+<IP Address> - [<date>] "<GET request>" <response status code> <file size>
 """
 
 
-def print_stats(size, status_codes):
-    """Print accumulated metrics
+def print_log_totals(total_file_size, code_counts):
     """
-    print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
+    Prints current totals of file size and status code counts.
+
+    Args:
+        total_file_size (int): cumulative total of bytes received through
+            GET requests in log
+        code_counts (OrderedDict): totals of status codes from repsonses
+    """
+    print("File size: {}".format(total_file_size))
+    for code in code_counts:
+        if code_counts[code] > 0:
+            print("{}: {}".format(code, code_counts[code]))
 
 
-if __name__ == "__main__":
-    import sys
+if __name__ == '__main__':
+    from sys import argv, stdin, stderr
+    from collections import OrderedDict
+    from datetime import datetime
 
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
+    line_no = 0
+    total_file_size = 0
+    code_counts = OrderedDict.fromkeys([200, 301, 400, 401, 403,
+                                        404, 405, 500], 0)
 
     try:
-        for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
+        for line in stdin:
+            line_no += 1
 
-            line = line.split()
+            # both ip addresses/URLs allowed, but some lines may be raw text
+            a = line.split('-', 1)
+            if len(a) != 2:
+                # likely not a formatted line
+                continue
 
+            # checking timestamp
+            b = a[1].split(']')
+            timecode = b[0].lstrip(' [')
             try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
+                datetime.strptime(timecode, '%Y-%m-%d %H:%M:%S.%f')
+            except:
+                stderr.write("{}: {}: invalid timecode\n".format(
+                    argv[0], line_no))
                 pass
 
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
-                pass
+            # checking URL
+            c = b[1].split('"')
+            c = c[1:]
+            if c[0] != 'GET /projects/260 HTTP/1.1':
+                stderr.write("{}: {}: unexpected HTTP request\n".format(
+                    argv[0], line_no))
 
-        print_stats(size, status_codes)
+            # prep for status code and file size
+            d = c[1].lstrip(' ')
+            d = d.rstrip('\n')
+            d = d.split(' ')
 
-    except KeyboardInterrupt:
-        print_stats(size, status_codes)
+            # checking status code (invalid codes skipped without error)
+            if d[0].isdecimal():
+                code = int(d[0])
+                code_counts[code] += 1
+
+            # checking file size (invalid sizes skipped without error)
+            if d[1].isdecimal():
+                total_file_size += int(d[1])
+
+            if line_no % 10 == 0:
+                print_log_totals(total_file_size, code_counts)
+        print_log_totals(total_file_size, code_counts)
+
+    except (KeyboardInterrupt):
+        print_log_totals(total_file_size, code_counts)
         raise
