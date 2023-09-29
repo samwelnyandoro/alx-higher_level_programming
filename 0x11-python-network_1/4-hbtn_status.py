f2c38cf7acf1abb19d442bdf3391032ea5f6ad47
Samwel Nyandoro <samwelnyandoro1@gmail.com>
2023-09-29 10:44:23 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,51 +1,10 @@
 #!/usr/bin/python3
-
-<<<<<<< HEAD
-"""
-
-requests model
-
-"""
-
-
-
-if __name__ == '__main__':
-    
-    import requests
-    
-    html = requests.get('https://alx-intranet.hbtn.io/status')
-    
-    print("Body response:")
-    
-    print("\t- type: {}".format(html.text.__class__))
-    
-    print("\t- content: {}".format(html.text))
-=======
-"""Takes in a URL, sends a request to the URL and"""
-
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
 
-
-
-
-def getStatus():
-    
-    req = requests.get('https://intranet.hbtn.io/status')
-    
-    content = req.text
-    
-
-    
-    print("Body response:")
-    
-    print("\t- type: {}".format(type(content)))
-    
-    print("\t- content: {}".format(content))
-    
-
-    
 if __name__ == "__main__":
-    
-    getStatus()
->>>>>>> a1cb096b6652ca13a4e5fb24e043d4fbe759e625
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
