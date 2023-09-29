9db4db76f133f4a12d514e4dc35650223a669a0f
Samwel Nyandoro <samwelnyandoro1@gmail.com>
2023-09-29 10:42:21 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,51 @@
 #!/usr/bin/python3
-"""Fetch URL using requests package"""
 
+<<<<<<< HEAD
+"""
+
+requests model
+
+"""
+
+
+
+if __name__ == '__main__':
+    
+    import requests
+    
+    html = requests.get('https://alx-intranet.hbtn.io/status')
+    
+    print("Body response:")
+    
+    print("\t- type: {}".format(html.text.__class__))
+    
+    print("\t- content: {}".format(html.text))
+=======
+"""Takes in a URL, sends a request to the URL and"""
+
+import requests
+
+
+
+
+
+def getStatus():
+    
+    req = requests.get('https://intranet.hbtn.io/status')
+    
+    content = req.text
+    
+
+    
+    print("Body response:")
+    
+    print("\t- type: {}".format(type(content)))
+    
+    print("\t- content: {}".format(content))
+    
+
+    
 if __name__ == "__main__":
-    from requests import get
-
-    try:
-        r = get('https://intranet.hbtn.io/status')
-        print('Body response:')
-        print('\t- type: {}'.format(type(r.text)))
-        print('\t- content: {}'.format(r.text))
-    except Exception as e:
-        print(e)
+    
+    getStatus()
+>>>>>>> a1cb096b6652ca13a4e5fb24e043d4fbe759e625
